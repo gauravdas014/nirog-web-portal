250be3386e0aa1312a2bfa2c3b4d6fa44a933738
Gaurav Das <gauravdas014@gmail.com>
2021-02-13 23:51:05 +0530

    update userModel with schema options

--- models/userModel.js
+++ models/userModel.js
@@ -1,21 +1,34 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    required: [true, 'Please tell us your name!'],
+    trim: true,
+    maxlength: [30, 'Your name must have less or equal then 30 characters'],
+    minlength: [10, 'Your name must have more or equal then 2 characters'],
   },
   email: {
     type: String,
+    required: [true, 'Please provide your email'],
+    // unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
   },
   phone: {
     type: String,
+    required: [true, 'Please tell us your phone number!'],
   },
   password: {
     type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
   },
   address: {
     type: String,
+    trim: true,
   },
   token: {
     type: String,
