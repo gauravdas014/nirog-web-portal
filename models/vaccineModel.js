d85447e65407f4fe45133e7770d782c6e0e628ea
Gaurav Das <gauravdas014@gmail.com>
2021-02-14 00:09:09 +0530

    update vaccine model

--- models/vaccineModel.js
+++ models/vaccineModel.js
@@ -4,24 +4,32 @@ const vaccineSchema = new mongoose.Schema(
   {
     name: {
       type: String,
+      trim: true,
+      required: [true, 'Please provide vaccine name'],
     },
     whenToGive: {
       type: String,
+      required: [true, 'Please specify the time to take the vaccine'],
     },
     dose: {
       type: String,
+      required: [true, 'Please specify the dose amount of the vaccine'],
     },
     smallDescription: {
       type: String,
+      trim: true,
     },
     description: {
       type: String,
+      trim: true,
     },
     route: {
       type: String,
+      required: true,
     },
     site: {
       type: String,
+      required: true,
     },
   },
   {
